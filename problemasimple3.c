3a5d7177a862f819e0e152265cf1de6ed1293204
aurorasolertovar <aurora.fi1234@gmail.com>
2026-05-01 09:31:57 -0600

    Create problemasimple3.c

---
+++ problemasimple3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    double dólares;
+    double pesos;
+    
+    printf("Cantidad de dólares: \n");
+    scanf("%lf", &dólares);
+    
+    pesos=dólares*11.96;
+    
+    printf("Cantidad de pesos: %lf \n",pesos);
+return 0;
+}
