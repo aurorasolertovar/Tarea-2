02880926da56e6fc56ce7270132559b1bdb11787
aurorasolertovar <aurora.fi1234@gmail.com>
2026-05-01 12:41:50 -0600

    Create problemasimple4.c

---
+++ problemasimple4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    double dinero;
+    double tasadeinterésmensual;
+    double ganancia;
+    
+    printf("Ingrese el dinero a invertir\n");
+    scanf("%lf", &dinero);
+    printf("Porcentaje de la tasa de interés mensual: \n");
+    scanf("%lf", &tasadeinterésmensual);
+    
+    ganancia=dinero*tasadeinterésmensual/100;
+    dinero=dinero+ganancia;
+    printf("Dinero a recibir: %lf", dinero);
+    
+return 0;    
+}
