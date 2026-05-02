0f5585b87125bb98cc59c7a069f504135e93fe23
aurorasolertovar <aurora.fi1234@gmail.com>
2026-05-01 18:51:29 -0600

    Create problemaconarreglo3.c

---
+++ problemaconarreglo3.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    int positivos=0;
+    int nulos=0;
+    int negativos=0;
+    int número;
+    int cont;
+    
+    printf("Ingresa la cantidad de números que quieres que tenga tu arreglo: \n");
+    scanf("%d", &n);
+    int vector [n];
+    cont=0;
+    while(cont<n)
+    {
+        printf("Valor %d: \n", cont+1);
+        scanf("%d", &número);
+        vector[cont]=número;
+        cont++;
+        if(número>0)
+        {
+            positivos++;
+        }
+        if(número==0)
+        {
+            nulos++;
+        }
+        if(número<0)
+        {
+            negativos++;
+        }
+    }
+    printf("Tu arreglo tiene %d números positivos,", positivos);
+    printf(" %d números nulos, y", nulos);
+    printf(" %d números negativos", negativos);
+
+return 0;
+}
